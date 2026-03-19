b53e7d6ad36ed1e89aa5bd959d8dc73eb045ae28
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-19 22:01:14 +0530

    feat: add Register route to AppRouter for user registration

--- src/Routing/AppRouter.jsx
+++ src/Routing/AppRouter.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Login from '../component/auth/Login'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default AppRouter
+export default AppRouter;
