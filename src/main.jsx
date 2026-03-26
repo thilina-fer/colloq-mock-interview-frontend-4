5eedb40d1c3be97b3f302fe3e213bf72be30efe7
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-26 21:33:59 +0530

    feat: add Google OAuth integration with jwt-decode and update main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google"; // Provider eka import kala
 
-createRoot(document.getElementById('root')).render(
+// Google Console eken gaththa Client ID eka methanata danna
+const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
