c7cdac4eda5cd7a353e40b297cc610cc0f028fe0
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-26 16:37:51 +0530

    feat: add Toaster component for improved user notifications in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from "react";
-import Logo from "./pages/auth/Login";
+import { Toaster } from "react-hot-toast";
 import AppRouter from "./Routing/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      {/* Toaster eka app eke uda harin thiyanne mehemai. 
+          Mulu app ekema ona thanaka dan toast pennanna puluwan! */}
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 3000, // Thathpara 3kin auto close wenawa
+          style: {
+            background: "#333", // Dark theme ekata match wena background eka
+            color: "#fff",
+            borderRadius: "8px",
+            fontSize: "14px",
+            fontWeight: "500",
+            border: "1px solid #444", // Podi border ekak lassanata
+          },
+          success: {
+            iconTheme: {
+              primary: "#ea580c", // Oyage theme eke orange color eka
+              secondary: "#fff",
+            },
+          },
+        }}
+      />
+
+      <AppRouter />
+    </>
+  );
 }
 
 export default App;
