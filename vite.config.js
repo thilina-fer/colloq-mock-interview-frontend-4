33df2dc588c5789a3179988b7fd57f5f4108fd59
thilina-fer <dilshanthilina1010@gmail.com>
2026-05-10 10:53:35 +0530

    feat: add lucide-react dependency for enhanced icon support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    headers: {
+      // Google Login popup එකේදී එන isolation warning එක fix කරන්න මේක අනිවාර්යයි
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+    },
+  },
+});
